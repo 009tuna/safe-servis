e71300089997c2e7479dc9110bd00d2cf7d644d4
SelimElibuyuk <selimqrwe@gmail.com>
2024-12-20 04:43:13 +0300

    Add files via upload

---
+++ Degerlendirmelerim_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class DegerlendirmelerimPage extends StatelessWidget {
+  const DegerlendirmelerimPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Degerlendirmelerim'),
+      ),
+      body: const Center(
+        child: Text('Degerlendirmelerim'),
+      )
+    );
+  }
+}
